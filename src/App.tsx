9f381c5e4c307fb983e052928ead3a7510ff73b5
killermasterturkey <killermasterturkey@users.noreply.github.com>
2025-12-29 15:27:11 +0300

    Add CallBanner to all pages via App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './router';
 import ScrollToTop from './components/ScrollToTop';
+import CallBanner from './components/feature/CallBanner';
 
 function App() {
   return (
     <BrowserRouter basename={__BASE_PATH__}>
       <ScrollToTop />
       <AppRoutes />
+      <CallBanner />
     </BrowserRouter>
   );
 }
